11364675aa96321047bda6eaa59d77eba4d36f30
Nicholas Ko <mailnickko@gmail.com>
2018-09-11 23:00:59 -0700

    implement queue

---
+++ dataStructures/__tests__/queue.spec.js
@@ -0,0 +1,34 @@
+const Queue = require('../queue.js');
+const expect = require('chai').expect;
+
+describe('Queues', () => {
+  let queue;
+  beforeEach(() => {
+    queue = new Queue();
+    queue.enqueue(1);
+    queue.enqueue(2);
+  });
+  describe('Queue', () => {
+    it('should have the methods enqueue(), dequeue(), size()', () => {
+      expect(queue.enqueue).to.be.a('function');
+      expect(queue.dequeue).to.be.a('function');
+      expect(queue.size).to.be.a('function');
+    });
+
+    it('should add values with enqueue', () => {
+      expect(queue.size()).to.be.equal(2);
+      queue.enqueue(3);
+      queue.enqueue(4);
+      expect(queue.size()).to.be.equal(4);
+    });
+
+    it('should remove values with dequeue and return the value', () => {
+      expect(queue.size()).to.be.equal(2);
+      var removed = queue.dequeue();
+      expect(removed).to.be.equal(1);
+      removed = queue.dequeue();
+      expect(removed).to.be.equal(2);
+      expect(queue.size()).to.be.equal(0);
+    });
+  });
+});
---
+++ dataStructures/queue.js
@@ -0,0 +1,27 @@
+class Queue {
+  constructor() {
+    this.firstKey = 0;
+    this.lastKey = 0;
+    this.storage = {};
+  }
+
+  enqueue(val) {
+    this.storage[this.lastKey] = val;
+    this.lastKey++;
+  }
+
+  dequeue() {
+    if (this.size()) {
+      const result = this.storage[this.firstKey];
+      delete this.storage[this.firstKey];
+      this.firstKey++;
+      return result;
+    }
+  }
+
+  size() {
+    return this.lastKey - this.firstKey;
+  }
+}
+
+module.exports = Queue;
