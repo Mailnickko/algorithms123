6d061f8ee50f1502ebd0bdc3c1fdf930447f5608
Nicholas Ko <mailnickko@gmail.com>
2018-09-11 23:01:59 -0700

    implement stack

---
+++ dataStructures/__tests__/stack.spec.js
@@ -0,0 +1,41 @@
+const Stack = require('../stack.js');
+const expect = require('chai').expect;
+
+describe('Stacks', () => {
+  let stack;
+  beforeEach(() => {
+    stack = new Stack();
+  });
+  describe('Stack', () => {
+    it('should have the methods push(), pop(), size()', () => {
+      expect(stack.push).to.be.a('function');
+      expect(stack.pop).to.be.a('function');
+      expect(stack.size).to.be.a('function');
+    });
+
+    it('should return the size of a stack', () => {
+      for (var i = 0; i < 15; i++) {
+        stack.push(1);
+      }
+
+      expect(stack.size()).to.equal(15);
+    });
+
+    it('should add values with push', () => {
+      expect(stack.size()).to.be.equal(0);
+      stack.push(1);
+      stack.push(2);
+      expect(stack.size()).to.be.equal(2);
+    });
+
+    it('should remove values with pop', () => {
+      expect(stack.size()).to.be.equal(0);
+      stack.push(1);
+      stack.push(2);
+      expect(stack.size()).to.be.equal(2);
+      var returned = stack.pop();
+      expect(stack.size()).to.be.equal(1);
+      expect(returned).to.be.equal(2);
+    });
+  });
+});
---
+++ dataStructures/stack.js
@@ -0,0 +1,19 @@
+class Stack {
+  constructor() {
+    this.storage = [];
+  }
+
+  push(val) {
+    this.storage.push(val);
+  }
+
+  pop() {
+    return this.storage.pop();
+  }
+
+  size() {
+    return this.storage.length;
+  }
+}
+
+module.exports = Stack;
