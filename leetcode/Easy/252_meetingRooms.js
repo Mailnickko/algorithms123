0d528ceb60febbb5bb9779ea9074388e1165e02b
Nicholas Ko <mailnickko@gmail.com>
2018-09-15 09:27:37 -0700

    implement meetingRooms

---
+++ leetcode/Easy/252_meetingRooms.js
@@ -0,0 +1,27 @@
+/*
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+Example 1:
+
+Input: [[0,30],[5,10],[15,20]]
+Output: false
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: true
+*/
+
+const canAttendMeetings = intervals => {
+  if (intervals.length < 2) {
+    return true;
+  }
+
+  const sorted = [...intervals].sort((a, b) => a.start - b.start);
+
+  for (const [index, curr] of sorted.entries()) {
+    if (index > 0 && curr.start < sorted[index - 1].end) {
+      return false;
+    }
+  }
+  return true;
+};
