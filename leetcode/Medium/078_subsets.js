00b510bb4dd59b7a088fe4e1847d00c530f6bb27
Nicholas Ko <mailnickko@gmail.com>
2018-09-21 03:14:19 -0700

    implement subsets

---
+++ leetcode/Medium/078_subsets.js
@@ -0,0 +1,47 @@
+/*
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
+const subsets = nums => {
+  const res = [[]];
+  nums.forEach(num => {
+    res.forEach(val => {
+      res.push([...val, num]);
+    });
+  });
+  return res;
+};
+
+// alternate backtracking
+const subsets2 = nums => {
+  const res = [];
+
+  const subroutine = (curr, remain, start) => {
+    res.push(curr);
+    for (let i = start; i < remain.length; i++) {
+      subroutine([...curr, remain[i]], [...remain.slice(0, i), ...remain.slice(i + 1)], start);
+      start++;
+    }
+  };
+
+  subroutine([], nums, 0);
+
+  return res;
+};
